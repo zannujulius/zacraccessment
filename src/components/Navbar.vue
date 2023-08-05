80bcf6c1059e6cbb85904b9acf3822bccbe3dbe4
zannujulius <zannujulius14@gmail.com>
2023-08-05 14:00:30 +0200

    completed responsiveness

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,11 +4,14 @@
             <div class="">
                 <img class="" src="@/assets/svg/applogo.svg" />
             </div>
-            <div class="flex items-center">
+            <div class="items-center hidden md:flex">
                 <router-link to="/" class="px-6 text-[14px] font-[400]">Home</router-link>
                 <router-link to="/" class="px-6 text-[14px] font-[400]">Our services</router-link>
                 <router-link to="/" class="px-6 text-[14px] font-[400]">Pricing</router-link>
                 <router-link to="/" class="px-6 text-[14px] font-[400]">Company</router-link>
+            </div>
+            <div class="flex md:hidden">
+                icon
             </div>
         </div>
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,10 @@
   <div class="">
    <Navbar />
    <!-- hero section -->
-   <div class="h-[600px] bg-primary grid grid-cols-2 overflow-hidden">
-      <div class="h-full flex flex-col justify-center px-20">
-        <h3 class="text-secondary text-[24px] font-[600]">Who We Are</h3>   
-        <p class="text-white">
+   <div class="h-screen lg:h-[600px] bg-primary md:h-screen grid md:grid-cols-1 lg:grid-cols-2 grid-cols-1 overflow-hidden">
+      <div class="h-full flex flex-col justify-center w-[90%] mx-auto md:px-20">
+        <h3 class="text-secondary text-[34px] font-[600]">Who We Are</h3>   
+        <p class="text-white font-light pt-6">
           Cashwise is a leading fintech company dedicated to providing personalized 
           financial services that cater to the unique needs and goals of our clients.
            With a team of industry experts, visionary technologists, and passionate financial 
@@ -13,44 +13,48 @@
           technological prowess.
         </p>   
       </div>
-      <div class="h-full flex flex-col justify-center relative">
-        <div class="absolute w-[140px h-[100px] bottom-[50px] left-[-150px]">
+      <div class="h-full flex flex-col justify-center relative ">
+        <!-- side -->
+        <div class="hidden md:block absolute z-30 md:h-[87px] md:bottom-6 md:left-[290px] lg:left-[-100px]">
           <img class="w-full h-full" src="@/assets/png/Frame_25.png" alt="" />
          </div>
-         <div class="absolute top-[70px] left-[190px]">
+         <!-- pointer -->
+         <div class="hidden md:absolute lg:block h-[60px] md:hidden w-[60px] md:left-40 z-30 top-[75px]">
             <img src="@/assets/svg/pointer.svg" alt="" class=""  />
           </div>
-        <div class="pt-[100px]">
+          <!-- main -->
+        <div class="absolute md:bottom-0 md:w-[500px] lg:w-auto md:right-0">
           <img src="@/assets/png/hero_img.png" alt="hero-img" />
         </div>
       </div>
     </div>
+ 
     <!-- section two -->
-    <div class="mx-auto grid mt-[100px] gap-6 grid-cols-2 w-[90%] justify-center items-center">
-        <div v-for="i in aboutData"  :key="i.id" class="h-[620px] bg-[#F3F3F3] flex flex-col justify-between rounded-[30px] p-10">
+    <div class="mx-auto grid mt-[100px] gap-10 md:gap-6 md:grid-cols-2 grid-cols-1 w-[90%] justify-center items-center">
+        <div v-for="i in aboutData" :key="i.id" class="h-auto md:h-[620px] bg-[#F3F3F3] flex flex-col justify-between rounded-[30px] p-6 md:p-10">
           <div class="flex itms-center justify-between">
-            <h3 class="font-[600] text-black text-[24px]">{{i.title}}</h3>
-            <div class="h-[40px] w-[40px]">
+            <h3 class="font-[600] text-black text-[22px] md:text-[24px]">{{i.title}}</h3>
+            <div class="h-[35px] w-[35px]">
               <img class="h-full w-full" src="@/assets/svg/arrow.svg" alt="" />
             </div>
           </div>
-          <div class="mx-auto h-[298px] w-[265px]" v-if="i.imageType == 'cube'">
+          <div class="mx-auto h-[250px] w-[220px] md:h-[298px] md:w-[265px]" v-if="i.imageType == 'cube'">
             <img src="@/assets/png/img11.png" class="w-full h-full" alt="" />
           </div>
-          <div class="mx-auto h-[298px] w-[265px]" v-if="i.imageType == 'cone'">
+          <div class="mx-auto  h-[250px] w-[220px] md:h-[298px] md:w-[265px]" v-if="i.imageType == 'cone'">
             <img src="@/assets/png/img10.png" class="w-full h-full" alt="" />
           </div>
-          <div class="text-black font-[400]">
+          <div class="text-black font-light">
             {{ i.desc }}
           </div>
         </div>
     </div>
     <!-- section three -->
     <div class="mx-auto mt-[100px] w-[90%] justify-center items-center">
-      <h3 class="text-[#0B150F] font-[600] text-[34px]">
+      <h3 class="text-[#0B150F] font-[600] text-[24px] md:text-[34px]">
         What Sets Us Apart
       </h3>
-      <div class="mt-10 grid gap-10 grid-cols-3">
+      <div class="mt-10 grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <div class="" v-for="i in nicheData" :key="i.id">
           <div class="h-[47px] w-[47px] flex items-center rounded-[10px] bg-lightPrimary  justify-center">
             <div class="h-[24px] w-[24px]" v-if="i.imgTag == 'bulb'">
@@ -80,21 +84,21 @@
     </div>
 
     <!-- section four  -->
-    <div class="mx-auto mt-[100px] relative bg-primary flex items-center overflow-hidden justify-center flex-col rounded-[30px] w-[90%] h-[439px]">
+    <div class="mx-auto mt-[100px] p-2 relative bg-primary flex items-center overflow-hidden justify-center flex-col rounded-[30px] h-auto w-[90%] md:h-[439px]">
       <div class="absolute">
         <img src="@/assets/svg/line_art.svg" alt="img" class="" />
       </div>
-      <h3 class="text-[#96EF76] text-[20px] headline font-Sora font-[500]" >
+      <h3 class="text-[#96EF76] pt-20 md:pt-0 text-center text-[20px] headline font-Sora font-[500]" >
         Join Us on Our Financial Journey  
       </h3>
-      <p class="text-center font-light w-[50%]  pt-10 text-white">
+      <p class="text-center font-light w-full md:w-[50%]  pt-6 text-white">
         We invite you to embark on this exciting financial journey with us. At [Your Fintech Company Name], we believe in a collaborative approach, where our clients become our partners. Together, we can shape a future where financial possibilities are limitless.
       </p>
-      <div class="flex items-center justify-center  mt-8">
-        <div class="w-[174px] h-[67px]">
+      <div class="flex-wrap justify-center flex md:flex md:items-center md:justify-center mt-8">
+        <div class="w-[174px] h-[67px] m-4">
           <img src="@/assets/png/Frame_334.png" alt="" class="w-full h-full" />
         </div>
-        <div class="w-[174px] h-[67px] ml-4">
+        <div class="w-[174px] h-[67px] m-4">
           <img src="@/assets/png/Frame_334.png" alt="" class="w-full h-full" />
         </div>
       </div>
@@ -102,13 +106,13 @@
   </div>
 
   <!-- section four -->
-  <div class="w-full bg-primary mt-[100px] h-[299px] ">
-    <div class="mx-auto w-[90%]  pt-10">
-      <div class="flex justify-between">
-        <div class="w-[137px] h-[27px]">
+  <div class="w-full bg-primary mt-[100px] h-[auto] md:h-[299px] ">
+    <div class="mx-auto w-[90%] pt-10">
+      <div class="block md:flex md:justify-between">
+        <div class="w-[137px] h-[27px] mb-10 md:mb-0">
           <img src="@/assets/png/app_logo.png" class="h-full w-full" alt="" />
         </div>
-        <div class="grid gap-16 grid-cols-3">
+        <div class="grid gap-16 grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
           <div class=""> 
             <h4 class="font-[600] text-white ">
               Support
@@ -153,6 +157,7 @@
       </p>
     </div>
   </div>
+
 </template>
 
 <script>
